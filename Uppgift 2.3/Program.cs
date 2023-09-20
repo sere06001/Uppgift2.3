190ec3f8250161f57effbc059d6c52c659ade8d4
sere06001 <sere06001@utb.vaxjo.se>
2023-09-20 10:29:50 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar tänker du hyra bilen?");
+            double dagar = double.Parse(Console.ReadLine());
+            double dagarSumma = 300 + (dagar - 1) * 500;
+            Console.WriteLine("Hur många km tänker du köra?");
+            double km = double.Parse(Console.ReadLine());
+            double kmSumma = km;
+            double totalaSumma = dagarSumma + kmSumma;
+            Console.WriteLine("Det kommer att kosta dig " + totalaSumma + "kr.");
+            Console.ReadKey();
+        }
+    }
+}
